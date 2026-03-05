7ff8f6d1fe858fd0b175ab253a7cab356539465b
Navabharath Technologies <dev@navabharathtechnologies.com>
2026-03-05 19:33:08 +0530

    Simplify vite.config.js per user request

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Nidhi-Fresh-Basket-Website/',
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: true
-  }
+  plugins: [react()]
 })
